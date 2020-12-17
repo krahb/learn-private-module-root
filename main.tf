1cd489859cbb01053bca91b92780ee7856407ae5
bob <bob.ter.hark@gmail.com>
2020-12-17 09:43:02 +0100

    Added private module on github

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
+module "s3-bobweb" {
+  source  = "app.terraform.io/krahb-home/s3-bobweb/aws"
   version = "1.0.0"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
 }
